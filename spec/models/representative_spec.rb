9f1afa3c49902fb961995305b85e473e0adcdfa9
Owen Thompson <owent@berkeley.edu>
2023-11-28 23:05:54 +0000

    [reps page] add rep test

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -3,19 +3,25 @@
 require 'rails_helper'
 
 describe Representative do
-  describe 'should not add duplicate representatives' do
-    official = OpenStruct.new({ name: 'Person' })
-    office = OpenStruct.new({ name: 'title', division_id: 'ocdid', official_indices: [0] })
-    rep_info = OpenStruct.new({ officials: [official], offices: [office] })
-    described_class.civic_api_to_representative_params(rep_info)
-    # should be one but the test is buggy
-    it 'is one' do
-      expect(described_class.count).to eq(0)
-    end
+  before do
+    @place = OpenStruct.new({ city: 'Berkeley', line1: '5 1st Street', state: 'CA', zip: '99999' })
+    @official = OpenStruct.new({ name: 'Person', address: [@place] })
+    @office = OpenStruct.new({ name: 'title', division_id: 'ocdid', official_indices: [0] })
+    @rep_info = OpenStruct.new({ officials: [@official], offices: [@office] })
+  end
 
-    described_class.civic_api_to_representative_params(rep_info)
-    it 'stills be one' do
-      expect(described_class.count).to eq(0)
-    end
+  it 'does not add duplicate representatives' do
+    described_class.civic_api_to_representative_params(@rep_info)
+    expect(described_class.count).to eq(1)
+    described_class.civic_api_to_representative_params(@rep_info)
+    expect(described_class.count).to eq(1)
+  end
+
+  it 'works if the address is valid' do
+    rep = described_class.civic_api_to_representative_params(@rep_info)[0]
+    expect(rep.attributes['city']).to eq('Berkeley')
+    expect(rep.attributes['street']).to eq('5 1st Street')
+    expect(rep.attributes['state']).to eq('CA')
+    expect(rep.attributes['zip']).to eq('99999')
   end
 end
