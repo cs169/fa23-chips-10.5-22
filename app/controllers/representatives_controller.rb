dcf4bb62838e36098ecb2a3aa033850fad20b0b4
Owen Thompson <owent@berkeley.edu>
2023-11-28 20:45:15 +0000

    basic rep stuff

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,15 +4,8 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
-  ### need a method here (search) to direct code to search.html.haml (whatever fucking view this is)
-  #def search
-    ##SearchController.search # 100% wrong
-    ##there may be something in params[:address] 
-    #address = params[:address] #### likely wrong 
-    #service = Google::Apis::CivicinfoV2::CivicInfoService.new
-    #service.key = Rails.application.credentials[:GOOGLE_API_KEY]
-    #result = service.representative_info_by_address(address: address)
-    #@representatives = Representative.civic_api_to_representative_params(result)
-    #render 'representatives/search' #### likely wrong 
-  #end
+
+  def show
+    @representative = Representative.find(params[:id])
+  end
 end
