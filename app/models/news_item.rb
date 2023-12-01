7b1f9ac036326169539978780c600c10d3f4fc2d
Rae Xin <raexin@berkeley.edu>
2023-12-01 07:47:56 +0000

    added frozen string literal line, changed enum to fit style check

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -4,7 +4,24 @@ class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
 
-  enum issues: ["Free Speech", "Immigration", "Terrorism", "Social Security and Medicare", "Abortion", "Student Loans", "Gun Control", "Unemployment", "Climate Change", "Homelessness", "Racism", "Tax Reform", "Net Neutrality", "Religious Freedom", "Border Security", "Minimum Wage", "Equal Pay"]
+  enum issues: 
+    ['Free Speech', 
+    'Immigration', 
+    'Terrorism', 
+    'Social Security and Medicare', 
+    'Abortion', 
+    'Student Loans', 
+    'Gun Control', 
+    'Unemployment', 
+    'Climate Change', 
+    'Homelessness', 
+    'Racism', 
+    'Tax Reform', 
+    'Net Neutrality', 
+    'Religious Freedom', 
+    'Border Security', 
+    'Minimum Wage', 
+    'Equal Pay']
 
   def self.find_for(representative_id)
     NewsItem.find_by(
