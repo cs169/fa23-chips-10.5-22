c350f0a427915caae481dcc5aa9ee15593d6f40e
Ricky Liu <rickyhl02@berkeley.edu>
2023-12-01 22:38:19 +0000

    dropdown logic implemented

--- app/controllers/campaign_finance_controller.rb
+++ app/controllers/campaign_finance_controller.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 class CampaignFinanceController < ApplicationController
-  def index; end
+  def index
+    @cycles_list = ["2010", "2012", "2014", "2016", "2018", "2020"]
+    @categories_list = ["candidate-loan", "contribution-total", "debts-owed", "disbursements-total", "end-cash", "individual-total", "pac-total", "receipts-total", "refund-total"]
+  end
 
-  def search; end
+  def search
+    api_key = Rails.application.credentials[:PROPUBLICA_API_KEY]
+    cycle = params[:search_terms][:cycle]
+    category = params[:search_terms][:category]
+    url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
+    connection = Faraday::Connection.new url
+    reply = connection.get do |req|
+      req.headers['X-API-Key'] = api_key
+    end
+    response = JSON.parse(reply.body)
+    puts response
+    #TODO Add results to an instance variable and render them on the search.html.haml page.
+  end
 end
