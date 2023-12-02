129be5bc1715baab494d94f62b1cadd49fc21d7f
Rae Xin <raexin@berkeley.edu>
2023-12-02 00:55:45 +0000

    change issues enum to hash format

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -4,25 +4,25 @@ class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
 
-  enum issues: [
-    'Free Speech',
-    'Immigration',
-    'Terrorism',
-    'Social Security and Medicare',
-    'Abortion',
-    'Student Loans',
-    'Gun Control',
-    'Unemployment',
-    'Climate Change',
-    'Homelessness',
-    'Racism',
-    'Tax Reform',
-    'Net Neutrality',
-    'Religious Freedom',
-    'Border Security',
-    'Minimum Wage',
-    'Equal Pay'
-  ]
+  enum issues: {
+    'Free Speech': 0,
+    'Immigration': 1,
+    'Terrorism': 2,
+    'Social Security and Medicare': 3,
+    'Abortion': 4,
+    'Student Loans': 5,
+    'Gun Control': 6,
+    'Unemployment': 7,
+    'Climate Change': 8,
+    'Homelessness': 9,
+    'Racism': 10,
+    'Tax Reform': 11,
+    'Net Neutrality': 12,
+    'Religious Freedom': 13,
+    'Border Security': 14,
+    'Minimum Wage': 15,
+    'Equal Pay': 16
+  }
 
   def self.find_for(representative_id)
     NewsItem.find_by(
