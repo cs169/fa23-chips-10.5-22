6efe3c485e377914763e1845456af2b70e6feb47
Garrett Moore <garrettmoore@berkeley.edu>
2023-12-04 10:51:18 +0000

    aa

--- app/controllers/campaign_finance_controller.rb
+++ app/controllers/campaign_finance_controller.rb
@@ -3,15 +3,20 @@
 class CampaignFinanceController < ApplicationController
   def index
     @cycles_list = %w[2010 2012 2014 2016 2018 2020]
-    @categories_list = %w[candidate-loan contribution-total debts-owed disbursements-total end-cash
-                          individual-total pac-total receipts-total refund-total]
+    @categories_list = %w[candidate-loan contribution-total debts-owed disbursements-total end-cash individual-total pac-total receipts-total refund-total]
   end
 
   def search
-    cycle = params[:search_terms][:cycle]
-    category = params[:search_terms][:category]
-    @campaign_finance_search_results = CampaignFinance.get_propublica_results(cycle, category)
+    @cycle = params[:search_terms][:cycle]
+    @category = params[:search_terms][:category]
+    @campaign_finance_search_results = CampaignFinance.get_propublica_results(@cycle, @category)
     redirect_to campaign_finance_path if @campaign_finance_search_results.nil?
-    # TODO: Use results of the @campaign_finance_search_results variable and render them on the search.html.haml page.
+    rep = []
+    @campaign_finance_search_results["results"].each do |r|
+      if r&.[]("name") && r&.[]("state") && r&.[]("party") && r&.[]("district") && r&.[]("total_from_individuals") && r&.[]("total_from_pacs") && r&.[]("total_contributions") && r&.[]("candidate_loans") && r&.[]("total_disbursements")
+        rep.push(r)
+      end
+    end
+    @campaign_finance_search_results = rep
   end
 end
