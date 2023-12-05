b6e822eee44dd9bb541bb86421f806a4fc47ddba
Owen Thompson <owent@berkeley.edu>
2023-12-05 18:21:47 +0000

    fix view linting and related test to work

--- app/controllers/campaign_finance_controller.rb
+++ app/controllers/campaign_finance_controller.rb
@@ -3,7 +3,8 @@
 class CampaignFinanceController < ApplicationController
   def index
     @cycles_list = %w[2010 2012 2014 2016 2018 2020]
-    @categories_list = %w[candidate-loan contribution-total debts-owed disbursements-total end-cash individual-total pac-total receipts-total refund-total]
+    @categories_list = %w[candidate-loan contribution-total debts-owed disbursements-total end-cash individual-total
+                          pac-total receipts-total refund-total]
   end
 
   def search
@@ -12,13 +13,8 @@ class CampaignFinanceController < ApplicationController
     @campaign_finance_search_results = CampaignFinance.get_propublica_results(@cycle, @category)
     redirect_to campaign_finance_path if @campaign_finance_search_results.nil?
     rep = []
-    @campaign_finance_search_results["results"].each do |r|
-      if r&.[]("name") && r&.[]("state") && r&.[]("party") && r&.[]("district") && r&.[]("total_from_individuals") && r&.[]("total_from_pacs") && r&.[]("total_contributions") && r&.[]("candidate_loans") && r&.[]("total_disbursements")
-        rep.push(r)
-      else
-        rep.push(r)
-        puts r
-      end
+    @campaign_finance_search_results['results'].each do |r|
+      rep.push(r)
     end
     @campaign_finance_search_results = rep
   end
--- app/models/county.rb
+++ app/models/county.rb
@@ -1,4 +1,4 @@
- # frozen_string_literal: true
+# frozen_string_literal: true
 
 class County < ApplicationRecord
   belongs_to :state
